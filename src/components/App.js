c1f69120c45e86ae9aad39203b522795849f3f61
Katherine Boedges <kboedges@me.com>
2018-06-07 09:51:32 -0500

    Adding a Nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Nav from './Nav'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+        <Nav/>
         Meow
       </div>
     );
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+
+  render() {
+    return (
+      <div className="Nav">
+        
+        <h1 className="nav-title">Readable</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
