54e86745a8981d10176c4308fc7c7e848dd092a8
Katherine Boedges <kboedges@me.com>
2018-06-07 11:51:58 -0500

    Adding styling and notes

--- src/components/App.js
+++ src/components/App.js
@@ -14,3 +14,9 @@ class App extends Component {
 }
 
 export default App;
+
+// If a selectedCategory exists, display categoryPosts instead of allPosts.
+// In the toolbar at the top, there will be an AddPost comp, CategorySelect, and SortPosts comp
+// Each tool comp will adjust the state in the store 
+// Based on the info in the store, ListPosts will display different information
+// The comps will work separately as much as possible
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,11 @@ class Nav extends Component {
 
   render() {
     return (
-      <div className="Nav">
-        
+      <div className="Nav d-flex flex-row justify-content-start align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" className="nav-icon align-self" width="32" height="32" viewBox="0 0 100 100">
+            <rect x="20" y="20" fill="transparent" stroke="#fff" strokeWidth="5" width="60" height="60"/>
+            <circle fill="transparent" stroke="#fff" cx="50" cy="50" r="45" strokeWidth="5"/>
+        </svg>
         <h1 className="nav-title">Readable</h1>
       </div>
     );
