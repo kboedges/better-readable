3405b467cb49d90ba41494c20224c72aec11a385
Katherine Boedges <kboedges@me.com>
2018-06-07 20:59:56 -0500

    Adding Posts

--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // Components
 import Post from './Post';
@@ -6,15 +7,26 @@ import Post from './Post';
 class ListPosts extends Component {
 
     render() {
+        const { allPosts } = this.props;
+
         return (
             <div className="ListPosts">
                 <ol className="post-list px-5 py-2">
-                    <li><Post/></li>
-                    <li><Post/></li>
+                {allPosts.map(post => (
+                    <li key={post.id}>
+                        <Post 
+                            post={post}
+                        />
+                    </li>
+                ))}
                 </ol>
             </div>
         );
     }
 }
 
-export default ListPosts;
+const mapStateToProps = ({allPosts}) => ({
+    allPosts,
+})
+
+export default connect(mapStateToProps)(ListPosts);
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Post extends Component {
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+  } 
 
   render() {
+    const { post } = this.props;
+
     return (
       <div className="Post mt-3">
         <div className="card">
             <div className="card-body">
-                <h4 className="card-title">Post title</h4>
-                <h6 className="card-subtitle mb-2 text-muted post-author">author</h6>
+                <h4 className="card-title">{post.title}</h4>
+                <h6 className="card-subtitle mb-2 text-muted post-author">{post.author}</h6>
                 <a href="#!" className="card-link ">Edit</a>
                 <a href="#!" className="card-link">Delete</a>
             </div>
