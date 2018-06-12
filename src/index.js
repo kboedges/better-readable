6189e925e9c7a4c5e1c602cfb85719c234fb5b8b
Katherine Boedges <kboedges@me.com>
2018-06-12 13:00:22 -0500

    Small adjustments

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 // Fetchers on load
-import { getAllPosts } from './reducers/posts/actions';
-import { getCategoryPosts } from './reducers/category-posts/actions';
-import { getCategories } from './reducers/categories/actions';
-import { getComment } from './reducers/comment/actions';
-// import { addPost } from './reducers/add-post/actions';
+import { getAllPosts } from "./reducers/posts/actions";
+import { getCategoryPosts } from "./reducers/category-posts/actions";
+import { getCategories } from "./reducers/categories/actions";
 
-const store = createStore(reducer, compose(
+const store = createStore(
+  reducer,
+  compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
-));
+  )
+);
 
 // Fetching everything on load
 store.dispatch(getAllPosts());
 store.dispatch(getCategoryPosts());
 store.dispatch(getCategories());
-store.dispatch(getComment());
-// store.dispatch(addPost());
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 registerServiceWorker();
