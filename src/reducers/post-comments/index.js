be5a9cb0c6e7591828832c139f6fcd4452ae7518
Katherine Boedges <kboedges@me.com>
2018-06-12 20:36:25 -0500

    Sorting comments

--- src/reducers/post-comments/index.js
+++ src/reducers/post-comments/index.js
@@ -5,7 +5,9 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_POST_COMMENTS:
-      return action.comments;
+      return action.comments.sort((a, b) => {
+        return b.timestamp - a.timestamp;
+      });
     default:
       return state;
   }
